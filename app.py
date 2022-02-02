5329a2b861c53b849acd6aa83f16bfff9f483c54
neha-sentieo <neha.singla@sentieo.com>
2022-02-02 10:55:30 +0530

    coupon code and payment functionality added in app

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from Uber.User.views import user_router
 from Uber.Vehicle.views import vehicle_router
 from Uber.ride.views import ride_router
+from Uber.Payments.views import payments_router
+from Uber.CouponCode.views import coupon_router
 
 app = FastAPI()
 
@@ -15,7 +17,9 @@ async def root():
 
 app.include_router(user_router)
 app.include_router(vehicle_router)
+app.include_router(coupon_router)
 app.include_router(ride_router)
+app.include_router(payments_router)
 
 
 if __name__ == "__main__":
